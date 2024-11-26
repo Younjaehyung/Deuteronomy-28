4c3d55433ad5fca7e2888b38bfb3db67693f8bb8
윤재형 DESKTOP-IFJTLG3\USER <104jake1217@gmail.com>
2024-11-26 22:16:45 +0900

    라이트 매니져

---
+++ OPENGL/LightManager.cpp
@@ -0,0 +1,32 @@
+﻿#include "LightManager.h"
+void LightManager::UpdateShadowMaps ( const std::vector<Object*>& sceneObjects ) {
+
+	glClear ( GL_DEPTH_BUFFER_BIT );
+	glEnable ( GL_CULL_FACE );
+	glCullFace ( GL_FRONT_FACE );
+
+
+	for ( auto& light : lightMass ) {
+		light->m_shadowMap->Bind ( );
+		glViewport ( 0 , 0 ,
+		light->m_shadowMap->GetShadowMap ( )->GetWidth ( ) ,
+		light->m_shadowMap->GetShadowMap ( )->GetHeight ( ) );
+		glClear ( GL_DEPTH_BUFFER_BIT );
+		m_simpleProgram->Use ( );
+
+		m_simpleProgram->SetUniform ( "color" , glm::vec4 ( 1.0f , 1.0f , 1.0f , 1.0f ) );
+
+
+
+		// 모든 오브젝트를 쉐도우맵에 렌더링
+		for ( auto* object : sceneObjects ) {
+			object->RenderShadow ( light->lightProjection * light->lightView, m_simpleProgram );
+		}
+
+		Framebuffer::BindToDefault ( );
+	}
+
+	glDisable ( GL_CULL_FACE );
+
+
+}
---
+++ OPENGL/LightManager.h
@@ -0,0 +1,100 @@
+﻿#pragma once
+#include "shadow.h"
+#include "object.h"
+#include "framebuffer.h"
+#include "shader.h"
+#include "program.h"
+#include "mesh_user.h"
+class LightManager
+{
+	//struct Light {
+	//	bool directional{ false };
+
+	//	glm::vec3 direction{ glm::vec3 ( -0.2f, -1.0f, -0.3f ) };	//Directional Light
+	//	glm::vec2 cutoff{ glm::vec2 ( 20.0f, 5.0f ) };	//Spot Light 보이는 부분
+	//	float distance{ 232.0f }; //Point Light 감쇠 거리
+
+	//	glm::vec3 position{ glm::vec3 ( 3.0f, 3.0f, 3.0f ) }; //광원의 위치
+	//	glm::vec3 ambient{ glm::vec3 ( 0.0f, 0.0f, 0.0f ) };  //광원의 색
+	//	glm::vec3 diffuse{ glm::vec3 ( 1.0f, 1.0f, 1.0f ) };  //오브젝트의 색
+	//	glm::vec3 specular{ glm::vec3 ( 1.0f, 1.0f, 1.0f ) };
+	//};
+
+	struct LightMass {
+		Light_ORI lightData;
+		glm::mat4 lightProjection;
+		glm::mat4 lightView;
+		ShadowMapUPtr m_shadowMap = ShadowMap::Create (2048,2048 );
+	};
+
+	const int MAXLIGHTNUM =20;
+	int LightNum = 0;
+	UBOBUFFER_LIGHTPtr UBOLight;
+	Program* m_simpleProgram;
+	std::vector<LightMass*> lightMass;
+	std::vector<Light_ORI> m_lights;
+	LightManager ( ) {
+
+	}
+public:
+	static LightManager& getInstance ( ) {
+		static LightManager instance;
+		return instance;
+	}
+
+	void UpdateShadowMaps ( const std::vector<Object*>& sceneObjects );
+
+	void Initialize (Program* program ) {
+		m_simpleProgram = program;
+
+		UBOLight = UBOBUFFER_LIGHT::Create ( MAXLIGHTNUM );
+
+	}
+
+	int GetLightNum ( ) {
+		return LightNum;
+	}
+
+	glm::mat4 GetLightTransform ( int i ) {
+		return lightMass[i]->lightProjection * lightMass[ i ]->lightView;
+	}
+
+	ShadowMap* GetShadowMap ( int i ) {
+		return lightMass[ i ]->m_shadowMap.get();
+	}
+
+
+	UBOBUFFER_LIGHTPtr GetLight ( ) {
+		return UBOLight;
+	}
+
+	void GetLightSetting (const Program* program ) {
+
+		UBOLight->Bind ( program->Get ( ) , "_lights" );
+		UBOLight->UpdateData ( m_lights );
+
+	}
+
+
+	void SetLight ( glm::vec3 pos , glm::vec3 dir , glm::vec2 cutoff , glm::vec3 color =glm::vec3(1.0f) , float distance = 232.f ) {
+		LightMass* lighting = new LightMass;
+		lighting->lightView= glm::lookAt ( pos ,pos + dir ,glm::vec3 ( 0.0f , 1.0f , 0.0f ) );
+
+		lighting->lightProjection = glm::perspective (
+	  glm::radians ( ( cutoff[ 0 ] + cutoff[ 1 ] ) * 2.0f ) ,1.0f , 1.0f , 20.0f );
+		lighting->lightData.position = pos;
+		lighting->lightData.direction = dir;
+		lighting->lightData.cutoff = cutoff;
+		
+		lighting->lightData.attenuation = GetAttenuationCoeff ( distance );
+		lighting->lightData.ambient = glm::vec3 ( 0.0f , 0.0f , 0.0f );
+		lighting->lightData.diffuse = color;
+		lighting->lightData.specular = glm::vec3 ( 1.0f , 1.0f , 1.0f );
+
+		m_lights.push_back ( lighting->lightData );
+		lightMass.push_back ( lighting );
+		LightNum = m_lights.size ( );
+	}
+
+};
+
