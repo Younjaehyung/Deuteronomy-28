35aa228eccadb941658d6eb0a180970f4c49f2fa
윤재형 DESKTOP-IFJTLG3\USER <104jake1217@gmail.com>
2024-12-04 02:49:42 +0900

    Deathmap

---
+++ OPENGL/Deathmap.cpp
@@ -0,0 +1,33 @@
+﻿#include "Deathmap.h"
+#include "LightManager.h"
+
+void Deathmap::Update ( )
+{
+
+
+
+}
+
+void Deathmap::Render ( const Program* program , glm::mat4 _cameraTransform )
+{
+	LightManager::getInstance ( ).GetLightSetting ( program );
+	m_model->Draw ( program );
+
+}
+
+void Deathmap::Initialize ( const std::string& strName )
+{
+	m_model = Model::Load ( strName );
+	if ( !m_model ) {
+		std::cerr << "program UserSetError id : " << m_model->Get ( ) << std::endl;
+		return;
+	}
+}
+
+void Deathmap::RenderShadow ( glm::mat4 lightView , const Program* program )
+{
+	program->SetUniform ( "modelTransform" , glm::translate ( glm::mat4 ( 1.0f ) , Pos ) );
+	program->SetUniform ( "transform" , lightView * glm::translate ( glm::mat4 ( 1.0f ) , Pos ) );
+	//std::cout << "model" << std::endl;
+	m_model->Draw ( program );
+}
---
+++ OPENGL/Deathmap.h
@@ -0,0 +1,26 @@
+﻿#pragma once
+#include "model.h"
+#include "Animation.h"
+#include "Animator.h"
+#include "CameraManager.h"
+#include "object.h"
+#include "LightMass.h"
+
+class Deathmap :public Object
+{
+	ModelPtr m_model;
+	Camera* mapCamera;
+	std::vector<LightMass*> Visullight;
+
+public:
+	Deathmap ( glm::vec3 _pos ) {
+		typeID = 0;
+		Pos = _pos;
+	}
+	virtual void Update ( );
+	virtual void Render ( const Program* program , glm::mat4 _cameraTransform );
+	virtual void Initialize ( const std::string& strName );
+	virtual void RenderShadow ( glm::mat4 lightView , const Program* program );
+
+};
+
